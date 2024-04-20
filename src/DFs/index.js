8ab5d042866d52fe3f8354ce71eda3bc07fbe6c0
Matrixx-Js <baptlaser.fortnite@gmail.com>
2024-04-21 00:53:22 +0200

    Login to BOT

--- src/DFs/index.js
+++ src/DFs/index.js
@@ -15,6 +15,8 @@ const DEFAULT_MAX_UPLOAD_CONCURRENCY = 3
 const { Client, Intents } = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 const { Collection } = require('discord.js');
+client.login(process.env.BOT_TOKEN)
+
 
 class DiscordFileSystem {
     constructor(opts) {
